9bac398ebf208d3b0e17ba1dd0151a60be4fffdb
Albert Hassey <ahasseyp97@gmail.com>
2023-12-13 21:20:23 -0600

    Create utils

--- utils/go.mod
+++ utils/go.mod
@@ -1,3 +1,5 @@
 module advent-of-code/utils
 
 go 1.21.5
+
+require github.com/dlclark/regexp2 v1.10.0
--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
-    "bufio"
-    "os"
+	"bufio"
+	"os"
+	"strconv"
+    "github.com/dlclark/regexp2"
 )
 
-// Helper function to read a file 
+// Helper function to read a file
 // Input is a file path
 // Output is a slice of type string
 func ReadFile(filePath string) []string {
@@ -25,4 +27,29 @@ func ReadFile(filePath string) []string {
     }
 
     return output
+}
+
+
+func StringSliceToIntegerSlice(lines []string) ([]int, error) {
+    integers := make([]int, 0, len(lines))
+
+    for _, line := range lines {
+        n, err := strconv.Atoi(line)
+        if err != nil {
+            return nil, err
+        }
+        integers = append(integers, n)
+    }
+    return integers, nil
+}
+
+
+func Regexp2FindAllString(re *regexp2.Regexp, s string) []string {
+	var matches []string
+	m, _ := re.FindStringMatch(s)
+	for m != nil {
+		matches = append(matches, m.String())
+		m, _ = re.FindNextMatch(m)
+	}
+	return matches
 }
